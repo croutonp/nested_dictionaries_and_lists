4af774d8736cc24488299aaf6ebccf86e175f9d6
crouton <116131622+croutonp@users.noreply.github.com>
2023-07-05 11:04:30 -0400

    Add files via upload

---
+++ nested_dictionaries_and_lists.py
@@ -0,0 +1,67 @@
+# ---1---
+
+# x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# x[1][0] = 15
+
+# students[0]['last_name'] = 'Bryant'
+
+# sports_directory ['soccer'][0] = 'Andres'
+
+# z[0]['y'] = 30
+
+# print(z)
+
+# ---2---
+
+#
+
+# def iterateDictionary(some_list):
+#     i = 0
+#     for i in range(0,len(some_list),1):
+#         print(some_list[i])
+
+ 
+
+
+
+
+# iterateDictionary(students) 
+
+# ---3---
+
+
+#     def iterateDictionary2(key_name, some_list):
+#         for i in range(len(some_list)):
+#         print(some_list[i][key_name])
+
+
+
+# iterateDictionary2('first_name', students)
+
+
+
+
+#---4---
+
+# dojo = {
+#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
+
+# def printInfo(element):
+#    for key in element:
+#        print(f"{len(element[key])} {key}")
+#        for i in range(len(element[key])):
+#            print(element[key][i])
+
+# printInfo(dojo)
